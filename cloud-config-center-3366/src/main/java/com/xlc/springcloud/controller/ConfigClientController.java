1ffeb58b1201a6e1cdb62fe54551670d40aebc86
xlc1016 <xlc101603@163.com>
2020-04-22 17:17:43 +0800

    添加新模块

---
+++ cloud-config-center-3366/src/main/java/com/xlc/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.xlc.springcloud.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+
+    @Value("${config.info}")
+    private String configInfo;
+
+
+    @GetMapping(value = "/configInfo")
+    public String configInfo(){
+
+        return  configInfo;
+    }
+}
